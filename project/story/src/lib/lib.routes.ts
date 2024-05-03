4fb0d3afc3c43fcfbae531a9cd1b6750e3881c74
Carlos N <machinery.e@gmail.com>
2024-05-03 21:20:12 +0000

    crear libreria de angular story

---
+++ project/story/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { StoryComponent } from './story/story.component';
+
+export const storyRoutes: Route[] = [{ path: '', component: StoryComponent }];
---
+++ project/story/src/lib/story/story.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ios-story-story',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './story.component.html',
+  styleUrl: './story.component.scss',
+})
+export class StoryComponent {}
