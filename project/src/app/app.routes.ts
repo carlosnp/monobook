58ec5294064144d5a810eb345abbffb530516d0a
Carlos N <machinery.e@gmail.com>
2024-05-03 21:59:09 +0000

    Crear modulos base

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+      },
+    ]
+  },
+];
---
+++ project/src/app/home/pages/init.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-init',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './init.component.html',
+  styleUrl: './init.component.scss',
+})
+export class InitComponent {}
