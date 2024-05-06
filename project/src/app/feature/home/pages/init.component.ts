720e7467710ab2cc24536e829af7cf7bd151b49f
Carlos N <machinery.e@gmail.com>
2024-05-06 19:15:32 +0000

    Implementar componente

--- project/src/app/feature/home/pages/init.component.ts
+++ project/src/app/feature/home/pages/init.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonMaterialComponent } from '@storybookHost';
 
 @Component({
   selector: 'app-init',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonMaterialComponent],
   templateUrl: './init.component.html',
   styleUrl: './init.component.scss',
 })
